d90c41fb8ffbb34d475d23f31d64c7f085c09077
Jays-t <j.j.h.traas@gmail.com>
2020-09-28 14:48:58 +0200

    centered battery icons

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+document.ready = function() {
+    toggleSections();
+  }
+
+function toggleSections() {
+    let amountScrolled =
+      document.getElementById("myBar").height();
+
+    let 
+
+    if amountScrolled === document.get
+
+  }
+
+  calculate links position on load, only re-calculate if screen size changes
