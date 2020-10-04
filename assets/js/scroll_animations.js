b9792b332791542ec80dda9734b8494076c5958d
Jays-t <j.j.h.traas@gmail.com>
2020-10-05 00:26:29 +0200

    scripts moved to js files + added svg filter to background

---
+++ assets/js/scroll_animations.js
@@ -0,0 +1,67 @@
+/*	NAVIGATION AND NAVIGATION VISIBLE ON SCROLL */
+
+$(window).on("load", function () 
+{
+  mainNav(); 
+});      
+
+$(window).scroll(function () 
+{
+    mainNav();
+  });
+  function mainNav() 
+  {
+    var top =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop;
+    if (top > 40)
+      $(".side-navbar").stop().animate({
+        opacity: "0.6",
+        top: "25%",
+      });
+    else
+      $(".side-navbar").stop().animate({
+        opacity: "0",
+        top: "45%",
+      });
+  }
+  
+window.onscroll = function () 
+{
+  progressBar();
+  highlight();
+};
+
+/* Fill progress bar */
+
+function progressBar() 
+{
+  let winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 50;
+  document.getElementById("myBar").style.height = scrolled + "%";
+}
+
+/* Fade in Highlight on scroll */
+
+function highlight() 
+{
+  let scroll = $(window).scrollTop();
+  let height = $(window).height();
+
+  $(".highlight").each(function () 
+  {
+    var pos = $(this).offset().top;
+    if (scroll + height >= pos) 
+    {
+      $(this).addClass("active");
+    } else {
+      $(this).removeClass("active");
+    }
+    console.log(pos);
+    console.log(scroll);
+  });
+}
