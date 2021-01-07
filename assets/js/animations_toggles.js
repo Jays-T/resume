690dceb71462c2f37eb8a7f2a8d3b832d8ffa40b
John Traas <j.j.h.traas@gmail.com>
2021-01-07 16:11:44 +0100

    condensed css to one file + removed unused code

--- assets/js/animations_toggles.js
+++ assets/js/animations_toggles.js
@@ -26,45 +26,45 @@
 //       });
 //   }
   
-// window.onscroll = function () 
-// {
-//   progressBar();
-//   highlight();
-// };
+window.onscroll = function () 
+{
+  progressBar();
+  // highlight();
+};
 
-// /* Fill progress bar and draw cat according to amount scrolled */
+/* Fill progress bar and draw cat according to amount scrolled */
 
-// function progressBar() 
-// {
-//   // get scroll + height
-//   let winScroll =
-//     document.body.scrollTop || document.documentElement.scrollTop;
-//   let height =
-//     document.documentElement.scrollHeight -
-//     document.documentElement.clientHeight;
+function progressBar() 
+{
+  // get scroll + height
+  let winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
 
-//   // Calculate scroll for progress bar
-//   let scrolled = (winScroll / height) * 52;
+  // Calculate scroll for progress bar
+  let scrolled = (winScroll / height) * 72;
 
-//   // Declare cat stroke offset
-//   const totalCat = 53765.1015625;
+  // // Declare cat stroke offset
+  // const totalCat = 53765.1015625;
 
-//   // Calculate scroll for cat drawing
-//   let catScroll = (winScroll / height) * 53765.1015625;
+  // // Calculate scroll for cat drawing
+  // let catScroll = (winScroll / height) * 53765.1015625;
 
-//   // Store cat length
-//   let theCat = document.getElementById("cat_path");
+  // // Store cat length
+  // let theCat = document.getElementById("cat_path");
 
-//   // Below commented out code allows to get total cat draw length if needed
-//   // let catLength = theCat.getTotalLength();
-//   // console.log(catLength);
+  // Below commented out code allows to get total cat draw length if needed
+  // let catLength = theCat.getTotalLength();
+  // console.log(catLength);
 
-//   // Draw progress bar in equal relation to percent scrolled
-//   document.getElementById("myBar").style.height = scrolled + "%";
+  // Draw progress bar in equal relation to percent scrolled
+  document.getElementById("myBar").style.height = scrolled + "%";
 
-//   // Draw cat in equal relation to percent scrolled
-//   theCat.style.strokeDashoffset = totalCat - catScroll;
-// }
+  // // Draw cat in equal relation to percent scrolled
+  // theCat.style.strokeDashoffset = totalCat - catScroll;
+}
 
 // /* Fade in Highlight on scroll */
 
