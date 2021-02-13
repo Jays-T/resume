bcf868b253d4104b5c93d553abe4f9199372b170
John Traas <j.j.h.traas@gmail.com>
2021-02-13 22:00:13 +0100

    email working removed redundant javascript

--- assets/js/form_validation.js
+++ assets/js/form_validation.js
@@ -1,93 +1,104 @@
+let formInputs = document.querySelectorAll('.form-input');
+const emailForm = document.getElementById('emailform');
+const emailSubmit = document.getElementById('email_button');
+const emailInput = document.getElementById('emailaddress');
 
-function myFunction(e) {
-    let isValidated = false;
+// Set focus on first input without value
+function setFocus() {
+    console.log(formInputs);
+    for (i = 0; i < formInputs.length; i++) {
+        if (!formInputs[i].value) {
+            formInputs[i].focus();
+            break;
+        }
+    }
+}
 
+// Checks if active input has value
+function isActive(e) {
+    let isValidated;
     let inputValue = document.activeElement.value;
     let activeInput = document.activeElement;
 
-
     if (inputValue != null) {
-        console.log(inputValue);
         isValidated = true;
-        console.log(isValidated + " should be true");
-        document.activeElement.nextElementSibling.classList.add("valid");
     }
 
-    console.log(inputValue + " inputValue before if statement");
-    console.log(activeInput.value + " active input value");
-    console.log(activeInput.defaultValue + " active input default value");
-    
     if (inputValue === activeInput.defaultValue){ 
         isValidated = false;
-        console.log(isValidated + "should be false");
     }
 
-    if (isValidated === false ) {
-        document.activeElement.nextElementSibling.classList.remove("valid");
-        document.activeElement.nextElementSibling.classList.add("active")
+    switch (isValidated) {
+        case true:
+            activeInput.nextElementSibling.classList.add("valid");
+            break;
+        case false:
+            activeInput.nextElementSibling.classList.remove("valid");
+            activeInput.nextElementSibling.classList.add("active");
+            break;
     }
+}
 
-    // if (activeInput != document.activeElement) {
+// Trigger animation for input currently in focus
 
-    // }
-  }
+formInputs.forEach(focus => focus.addEventListener('focusout', () => {
+    focus.nextElementSibling.classList.remove("active");
+}));
 
-
+// Prevent default browser form invalid input popup
 document.querySelector( "#visitorname" )
     .addEventListener( "invalid", function( e ) {
         e.preventDefault();
-        // console.log(e);
-        // e.target.nextElementSibling.classList.add("active");
     }, true );
     document.querySelector( "form" )
     .addEventListener( "invalid", function( e ) {
         e.preventDefault();
     }, true );
 
-// document.querySelector("#emailform").addEventListener("valid", function (e){
-//     e.target.nextElementSibling.classList.remove("active");
-// });
+// Validate email syntax on submit
+emailForm.addEventListener('submit', (e) => {
+    let visitorEmail = emailInput.value;
+    let validation = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    let emailValidated = false;
+    const service_id = "angry_cat_ao@heih!1";
+    const template_id = "portfolio_cX69AB4A";
 
+    e.preventDefault();
 
-    // function replaceValidationUI( form ) {
-    //     // Suppress the default bubbles
-    //     form.addEventListener( "invalid", function( event ) {
-    //         event.preventDefault();
-    //     }, true );
-    
-    //     // Support Safari, iOS Safari, and the Android browser—each of which do not prevent
-    //     // form submissions by default
-    //     form.addEventListener( "submit", function( event ) {
-    //         if ( !this.checkValidity() ) {
-    //             event.preventDefault();
-    //         }
-    //     });
-    
-    //     var submitButton = form.querySelector( "button:not([type=button]), input[type=submit]" );
-    //     submitButton.addEventListener( "click", function( event ) {
-    //         var invalidFields = form.querySelectorAll( ":valid" ),
-    //             errorMessages = form.querySelectorAll( ".error-message" ),
-    //             parent;
-    
-    //         // Remove any existing messages
-    //         for ( var i = 0; i < errorMessages.length; i++ ) {
-    //             errorMessages[ i ].parentNode.removeChild( errorMessages[ i ] );
-    //         }
-    
-    //         for ( var i = 0; i < invalidFields.length; i++ ) {
-    //             parent = invalidFields[ i ].parentNode;
-    //             parent.insertAdjacentHTML( "beforeend", "<div class='error-message'></div>" );
-    //         }
-    
-    //         // If there are errors, give focus to the first invalid field
-    //         if ( invalidFields.length > 0 ) {
-    //             invalidFields[ 0 ].focus();
-    //         }
-    //     });
-    // }
-    
-    // // Replace the validation UI for all forms
-    // var forms = document.querySelectorAll( "form" );
-    // for ( var i = 0; i < forms.length; i++ ) {
-    //     replaceValidationUI( forms[ i ] );
-    // }
+    if (visitorEmail.match(validation)) {
+        emailValidated = true;
+    } else {
+        emailValidated = false;
+    }
+    switch (emailValidated) {
+        case false:
+            console.log("No no no, no emails today!");
+            emailInput.focus();
+            emailInput.nextElementSibling.classList.remove('valid');
+            emailInput.nextElementSibling.classList.add('active');
+            break;
+        case true:
+            console.log("Yeeeeeeees, emails today!");
+            emailjs.sendForm(service_id, template_id, emailForm);
+            emailInput.nextElementSibling.classList.remove('active');
+            emailForm.reset();
+            break;
+    }
+    return false;
+});
+
+// event.preventDefault();
+
+//   var service_id = "everyday_gourmet";
+//   var template_id = "everyday_gourmet_email";
+
+//   myform.find("button").text("Sending...");
+//   emailjs.sendForm(service_id,template_id,myform[0])
+//   	.then(function(){
+//         myform.find("button").text("Sent!");
+//     }, function(err) {
+//        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+//        myform.find("button").text("Retry");
+//     });
+//     document.getElementById('emailform').reset();
+//   return false;
